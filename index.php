326b752730f3da4a828bef15ec0fba7e61081e66
bukandicki <67890542+bukandicki@users.noreply.github.com>
2020-08-11 23:16:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/master.css" type="text/css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/master.js"></script>
+    <title>Redeem Garena 2018</title>
+  </head>
+  <body>
+    <header>
+      <div class="nav-container">
+        <div class="logo">
+          <img src="img/logo.png" alt="" style="height: 3em;">
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Redeem</a></li>
+            <li><a href="#">History</a></li>
+            <li><a href="#" class="active">Login</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/nd.jpg" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/Kriss_SV_Cupid.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/Kriss_SV_4th_Anniversary.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/KrissSuperV_GSL2016.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/riss S.V PBSC 2014.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/1400813335396_.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/AUG_A3_Cupid.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/Cheytac_M200_Cupid.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/1402908214103_.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/51.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/M1887_Cupid.png" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+          <div class="animated fadeInUp item-gun">
+            <div class="item-gun-image">
+              <img src="img/688a6-amokpalmahutabarat.gif" alt="" width="100%">
+            </div>
+            <span>2018 Gratis!!!</span>
+            <button class="ambil" type="button" name="button">Ambil</button>
+          </div>
+    </section>
+  </body>
+</html>
